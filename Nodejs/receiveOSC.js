6e01f332d512be670e923291c60d94b26d7fd6ba
Fabi <fabi.images@gmail.com>
2016-03-10 17:16:04 +0100

    initial commit

---
+++ Nodejs/receiveOSC.js
@@ -0,0 +1,33 @@
+// tested with vvvv, TouchOSC
+
+var osc = require('osc-min');
+var udp = require("dgram");
+var inport = 8000;
+
+console.log("OSC listener running at http://localhost:" + inport);
+
+// A simple OSC printer;
+
+var sock = udp.createSocket("udp4", function (msg) {
+    var error, error1;
+    try {
+        return console.log(osc.fromBuffer(msg));
+    }
+    catch (error1) {
+        error = error1;
+        return console.log("invalid OSC packet");
+    }
+});
+
+sock.bind(inport);
+
+// logs e. g.
+//{
+//    address: '/1/fader1',
+//    args: [{
+//        type: 'float',
+//        value: 0.7665270566940308
+//    }],
+//    oscType:'message'
+//}
+
